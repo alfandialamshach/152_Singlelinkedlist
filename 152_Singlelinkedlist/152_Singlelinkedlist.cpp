a689f6d94e05648cea5a0efdd2b1c9def18d485d
alfandi <fandidixpc1@gmail.com>
2023-05-19 18:29:26 +0700

    1

--- 152_Singlelinkedlist/152_Singlelinkedlist.cpp
+++ 152_Singlelinkedlist/152_Singlelinkedlist.cpp
@@ -21,7 +21,7 @@ void addNode() {
 	nodeBaru->name = nama;
 
 	if (START == NULL || nim <= START->noMhs) 
-	{
+	{ 
 		if (START != NULL && nim == START->noMhs) {
 			cout << " NIM sudah ada " << endl;
 			return;
