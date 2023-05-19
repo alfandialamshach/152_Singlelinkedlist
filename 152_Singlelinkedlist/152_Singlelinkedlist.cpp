45c35f5a7e907568921422b2fc4ee4632861afda
alfandi <fandidixpc1@gmail.com>
2023-05-19 11:13:54 +0700

    Membuat procedure untuk menambah data

--- 152_Singlelinkedlist/152_Singlelinkedlist.cpp
+++ 152_Singlelinkedlist/152_Singlelinkedlist.cpp
@@ -11,5 +11,17 @@ struct Node
 Node* START = NULL;
 
 void addNote() {
-	int nim
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukkan NIM: ";
+	cin >> nim;
+	cout << "Masukkan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START ->)
+	}
 }
