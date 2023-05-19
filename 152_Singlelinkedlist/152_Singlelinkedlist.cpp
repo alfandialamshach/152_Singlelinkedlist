aa6ca6613249a8541174824082643b898a3f4d48
alfandi <fandidixpc1@gmail.com>
2023-05-19 17:18:18 +0700

    Membuat procedure traverse

--- 152_Singlelinkedlist/152_Singlelinkedlist.cpp
+++ 152_Singlelinkedlist/152_Singlelinkedlist.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-struct Node
-{
+struct Node {
 	int noMhs;
 	string name;
 	Node* next;
@@ -21,7 +20,8 @@ void addNote() {
 	nodeBaru->noMhs = nim;
 	nodeBaru->name = nama;
 
-	if (START == NULL || nim <= START->noMhs) {
+	if (START == NULL || nim <= START->noMhs) 
+	{
 		if (START != NULL && nim == START->noMhs) {
 			cout << " NIM sudah ada " << endl;
 			return;
@@ -36,7 +36,65 @@ void addNote() {
 
 		while ((current != NULL) && (nim >= current->noMhs))
 		{
+			if (nim == current->noMhs)
+			{
+				cout << "NIM sudah ada" << endl;
+				return;
+			}
+			previous = current;
+			current = current->next;
 
 		}
+	
+		nodeBaru->next = current;
+		previous->next = nodeBaru;
+}
+
+bool serachNode(int nim, Node* current, Node* previous) {
+	previous = START;
+	current = START;
+	while (current != NULL && nim > current->noMhs)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
+		return false;
+	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+bool deleteNode(int nim) {
+	Node* current = START;
+	Node* previous = START;
+	if (serachNode(nim, previous, current) == false)
+		return false;
+	previous->next = current->next;
+	if (current == START)
+		START = current->next;
+	return true;
+}
+bool listEmpety() {
+	if (START == NULL)
+		return false;
+	else
+		return false;
+
+}
+
+void traverse() {
+	if (listEmpety()) {
+		cout << "List Kosong" << endl;
+		system("pause");
+		system("cls");
+		return;
 	}
 }
